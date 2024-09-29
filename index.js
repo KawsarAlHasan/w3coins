54010468dd4935a0e0f6411f4374eb17f9ee9e02
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2024-09-29 16:07:23 +0600

    updeted some file

--- index.js
+++ index.js
@@ -55,3 +55,10 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.status(200).send("W3Coins Server is working");
 });
+
+// 404 Not Found middleware
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    error: "You have hit the wrong route",
+  });
+});
